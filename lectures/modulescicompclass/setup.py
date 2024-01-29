942ea77335525fc3ab670d576fdaac553bd80c63
Davide Gerosa <davide.gerosa@gmail.com>
2024-01-29 07:43:12 +0100

    Mon Jan 29 07:43:12 CET 2024

--- lectures/modulescicompclass/setup.py
+++ lectures/modulescicompclass/setup.py
@@ -6,6 +6,6 @@ setup(name='modulescicompclass',
       author='Davide Gerosa',
       author_email='davide.gerosa@unimib.it',
       license='MIT',
-      version='0.0.1',
+      version='0.0.3',
       packages=find_packages(),
       install_requires=['numpy', 'matplotlib'])
